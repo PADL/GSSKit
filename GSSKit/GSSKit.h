e563d178472bd0e6f0dbcd06d42d652ffeead68e
Luke Howard <lukeh@padl.com>
2013-11-07 07:43:34 +1100

    Initial Commit

---
+++ GSSKit/GSSKit.h
@@ -0,0 +1,13 @@
+//
+//  GSSKit.h
+//  GSSKit
+//
+//  Created by Luke Howard on 7/11/2013.
+//  Copyright (c) 2013 PADL. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GSSKit : NSObject
+
+@end
---
+++ GSSKit/GSSKit.m
@@ -0,0 +1,13 @@
+//
+//  GSSKit.m
+//  GSSKit
+//
+//  Created by Luke Howard on 7/11/2013.
+//  Copyright (c) 2013 PADL. All rights reserved.
+//
+
+#import "GSSKit.h"
+
+@implementation GSSKit
+
+@end
