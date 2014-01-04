62e0d0ae7543de552b0951d2c1983be70685c65c
Luke Howard <lukeh@padl.com>
2014-01-04 16:02:03 +1100

    logging

--- GSSKit/NSArray+GSSOIDHelper.m
+++ GSSKit/NSArray+GSSOIDHelper.m
@@ -19,14 +19,21 @@
     for (OM_uint32 i = 0; i < oids->count; i++) {
         gss_OID thisOid = &oids->elements[i];
 
-        if (thisOid->elements == NULL)
+        if (thisOid->elements == NULL) {
+            NSLog(@"+[NSArray(GSSOIDHelper) arrayWithGSSOIDSet:]: NULL OID in %p", thisOid);
             continue;
+        } else if (thisOid->length == 0) {
+            NSLog(@"+[NSArray(GSSOIDHelper) arrayWithGSSOIDSet:]: zero length OID in %p", thisOid);
+            continue;
+        }
 
         NSData *derData = [NSData dataWithBytes:thisOid->elements length:thisOid->length];
         GSSMechanism *mech = [GSSMechanism mechanismWithDERData:derData];
 
         if (mech != nil)
             [array addObject:mech];
+        else
+            NSLog(@"+[NSArray(GSSOIDHelper) arrayWithGSSOIDSet:]: could not parse mechanism");
     }
     
     return array;
